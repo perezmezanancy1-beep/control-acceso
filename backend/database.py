490c337a8a023082bb97fad50b2ca0c93dbd98ba
perezmezanancy1-beep <perezmezanancy1@gmail.com>
2026-06-04 17:00:17 -0500

    Update database.py

--- backend/database.py
+++ backend/database.py
@@ -3,16 +3,20 @@ from firebase_admin import credentials, firestore
 import json
 import os
 
-# ✅ Inicializar Firebase correctamente en Render
+# ✅ Inicializar Firebase correctamente
 if not firebase_admin._apps:
 
     if "FIREBASE_CREDENTIALS" in os.environ:
-        # Render / Producción
+        # ✅ Producción (Render)
         cred_dict = json.loads(os.environ["FIREBASE_CREDENTIALS"])
         cred = credentials.Certificate(cred_dict)
     else:
-        # Local
-cred = credentials.Certificate("backend/serviceAccountKey.json")
-firebase_admin.initialize_app(cred)
+        # ✅ LOCAL (TU PC)
+        cred = credentials.Certificate(
+            r"C:\Users\PC HP 14-DY0005LA\OneDrive - UNIVERSIDAD AUTONOMA DEL CARIBE\Desktop\control_acceso\backend\serviceAccountKey.json"
+        )
 
+    firebase_admin.initialize_app(cred)
+
+# ✅ Conexión a Firestore
 db = firestore.client()
